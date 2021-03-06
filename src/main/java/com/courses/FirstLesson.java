ce2acf79101c18a645abec818e0fbf5748da874c
JulesAuz <juliaauzdk@gmail.com>
2021-03-06 19:31:40 +0100

    First part of the code

---
+++ src/main/java/com/courses/FirstLesson.java
@@ -0,0 +1,34 @@
+package com.courses;
+
+public class FirstLesson {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        boolean isVisible = true;
+        boolean isNotVisible = false;
+
+        // 8 bits from -128 to 127
+        byte bt = 127;
+
+        // 16 bits from -32768 to 32767
+        short shrt =32767;
+
+        // 32 bits from -2,147,483,648 to 2,147,483,647
+        int integer = 2147483647;
+
+        // 64 bits from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        long lng = 9223372036854775807L;
+
+        // 32 bits from 1.23e100f, -1.23e-100f, .3f, 3.14F
+        float flt = 2.5F;
+
+        // 64 bits from 1.7976931348623157 x 10308, to 4.9406564584124654 x 10-324
+        double dbl = 8.5;
+
+        //16 bits Unicode character
+        char cr = '&';
+    }
+
+
+
+}
