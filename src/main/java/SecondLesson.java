80941020194dea5fd96f6366c476c969e205f503
JulesAuz <juliaauzdk@gmail.com>
2021-03-07 21:04:38 +0100

    Trying again

--- src/main/java/SecondLesson.java
+++ src/main/java/SecondLesson.java
@@ -4,9 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-
-import java.awt.*;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class SecondLesson {
 
@@ -15,7 +14,9 @@ public class SecondLesson {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com");
+        driver.manage().window().maximize();
 
+        driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
         WebElement usernameField = driver.findElement(By.className("form_input"));
         WebElement passwordField = driver.findElement(By.xpath("//input[2]"));
         WebElement loginButton = driver.findElement(By.xpath("//input[@type='submit']"));
@@ -24,17 +25,22 @@ public class SecondLesson {
         passwordField.sendKeys("secret_sauce");
         loginButton.click();
 
-        List<WebElement> list = driver.findElements(By.xpath("//button"));
+        List<WebElement> list = driver.findElements(By.className("btn_primary btn_inventory"));
         WebElement addToCart = list.get(1);
         addToCart.click();
 
         WebElement filter = driver.findElement(By.className("product_sort_container"));
         Select select = new Select(filter);
 
-        select.selectByVisibleText("Name (Z to A)");
-        addToCart = list.get(2);
-        addToCart.click();
+        List<WebElement> listOfOptions = select.getOptions();
+        for (WebElement listOfOption : listOfOptions)
+            System.out.println(listOfOption.getText());
 
-        driver.quit();
+            select.selectByVisibleText("Name (Z to A)");
+            addToCart = list.get(2);
+            addToCart.click();
+
+            driver.quit();
+
     }
 }
